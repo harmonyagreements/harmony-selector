66740a848f8216386213fb3355015cc5624f6611
Allison Randal <allison@lohutok.net>
2011-07-03 23:11:49 +0000

    Setting selector as the default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Harmony::Application.routes.draw do
+  get "selector/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +51,7 @@ Harmony::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+   root :to => "selector#index"
 
   # See how all your routes lay out with "rake routes"
 

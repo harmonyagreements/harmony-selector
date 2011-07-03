6d3ab1e423cc244437a8beec7a5850fa09fc6e24
Allison Randal <allison@meep.(none)>
2011-07-03 20:51:27 +0000

    Empty rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harmony::Application.config.secret_token = 'ef2efc9a8284bc0cf70955434bab1a6b89f250a15533e05ba1c6bb35a09217f47b4a81617fbc224e51bf695e705d7a3338505f5e573105cdb278c1d7c3dbd89f'
